8680682bd30ed89268c16788828fcee72a830f88
iam-hbk <rainheritier@gmail.com>
2024-02-17 18:30:21 +0200

    Update nav-bar component and fix typo in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Urbanist as FontSans } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/navbar";
+import { Navbar } from "@/components/nav-bar";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 
