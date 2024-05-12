05c7df284990f8612891766e706daa3610cd6677
iam-hbk <rainheritier@gmail.com>
2024-05-12 19:42:55 +0200

    made landing page - section 1 responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,14 +27,14 @@ function Page({}: Props) {
       className={"flex min-h-screen flex-col items-center justify-center"}
     >
       <div className="flex h-screen w-full flex-col-reverse items-center justify-center bg-gradient-to-tr from-gray-300 to-secondary p-5 dark:from-muted md:flex-row md:justify-around md:p-20">
-        <div className="flex h-auto flex-col items-center justify-center gap-5 ">
+        <div className="flex h-auto flex-col items-center justify-center gap-10">
           <h1 className=" text-4xl tracking-tight lg:text-5xl">
             Recruitment Agency
           </h1>
           <h1 className="font-script scroll-m-20 text-4xl tracking-tight lg:text-5xl">
             Niche Specialisation
           </h1>
-          <p className="max-h-[20%] text-center md:max-w-[50%] md:text-left">
+          <p className="max-h-[20%] text-center md:max-w-[65%] md:text-left">
             Welcome to Into Being Placements we are a specialised headhunting
             and talent acquisition agency. Providing you with recruitment and
             human capital solutions for your business. Let our talent find you
@@ -48,20 +48,11 @@ function Page({}: Props) {
         </div>
 
         <Image
-          priority // keep only if this image is critical for initial loading
-          src={Logo} // ensure Logo is imported and optimized
-          alt="logo"
-          className="h-auto w-1/6 min-w-[130px] max-w-[400px] rounded-full sm:w-1/3 md:w-1/2 lg:w-full" // added default width for xs screens
-        />
-
-        {/* <Image
           priority
           src={Logo}
           alt="logo"
-          height={500}
-          width={500}
-          className="lg:w-full h-auto rounded-full sm:w-1/5 md:w-1/3"
-        /> */}
+          className="h-auto w-1/6 min-w-[130px] max-w-[400px] rounded-full sm:w-1/3 md:w-1/2 lg:w-full" // added default width for xs screens
+        />
       </div>
       {/* Services Section */}
       <Services />
