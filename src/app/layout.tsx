985c1a86c84df57fe4a47b4f9460ff4f9c99a554
iam-hbk <rainheritier@gmail.com>
2024-05-12 21:24:53 +0200

    made footer responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
             <MobileNav />
           </nav>
           <main>{children}</main>
-          {/*<Footer />*/}
+          <Footer />
           <Toaster richColors />
         </ThemeProvider>
       </body>
